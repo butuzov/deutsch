0841a92b2b4bcc20fb850e7c6dc3db70574211d1
Oleg Butuzov <butuzov@made.ua>
2020-03-25 07:07:03 +0200

    (anki): present_indicative update

--- duolingo/001_verbs.py
+++ duolingo/001_verbs.py
@@ -1,9 +1,12 @@
+# This file is crating a verb card with easydecks module for duolingo deutsch course verbs from
+# level 1.
+#
 from enum import Enum, auto
 import re
 import easydecks as ed
 from random import choice, shuffle
 
-# Verbs in Present Indicative form
+# actual functionality
 
 
 def strip_tags(value):
@@ -25,11 +28,12 @@ class Pronoun(Enum):
             self.Er_Sie_Es: ['Er', 'Sie', 'Es'],
             self.Wir: ['Wir'],
             self.Ihr: ['Ihr'],
-            self.Sie: ['Sie'],
+            self.Sie: ['Sie']
         }[self])
 
 
-def forms(verb, extra={}):
+def present_indicative(verb, extra={}):
+    """ Form a forms of the present indicative tense """
     data = {}
     endings = {
         Pronoun.Ich: 'e',
@@ -40,22 +44,14 @@ def forms(verb, extra={}):
         Pronoun.Sie: 'en',
     }
 
-    base = verb[:-2]
     for pron, ends in endings.items():
         if pron in extra:
             data[pron] = '<span class="irregular">%s</span>' % extra[pron]
         else:
             data[pron] = '<span class="base">%s</span><span class="changed">%s</span>' % (
-                base, ends)
+                verb[:-2], ends)
 
     return data
-
-
-Irregular = {
-    'essen': 'to eat', # du: isst, er/sie/es: isst
-    'lesen': 'to read', # du 	liest, er/sie/es: liest
-    'haben': 'to have', # du hast, es/sie/es: hat
-}
 
 
 def to_struct(translation: str, addition: str, is_regular: bool, extra={}):
@@ -67,41 +63,48 @@ def to_struct(translation: str, addition: str, is_regular: bool, extra={}):
     }
 
 
+# Verbs in Level 1 Duolingo Deutsch
+
 Varbs = {
-    'essen': to_struct('to eat', 'Brot', False, {
-        Pronoun.Du: 'isst',
-        Pronoun.Er_Sie_Es: 'isst',
-    }),
     'kommen': to_struct('to come', 'aus Deutschland', True),
     'trinken': to_struct('to drink', 'wasser', True),
     'verstehen': to_struct('to understand', 'es nicht', True),
+    'sprechen': to_struct('to speak', 'Deutsch', True),
     'lesen': to_struct(
         'to read', 'Zeitung', False, {
             Pronoun.Du: 'liest',
             Pronoun.Er_Sie_Es: 'liest',
         }),
+    'essen': to_struct('to eat', 'Brot', False, {
+        Pronoun.Du: 'isst',
+        Pronoun.Er_Sie_Es: 'isst',
+    }),
+    'haben': to_struct('to have', 'Brot', False, {
+        Pronoun.Du: 'hast',
+        Pronoun.Er_Sie_Es: 'hat',
+    }),
 }
 
-data = []
-for verb, item in Varbs.items():
-    for pron, form in forms(verb).items():
-        verb_form = form if pron not in item['extra'] else item['extra'][pron]
-        sentence_q = "%s ... %s" % (pron, item['addition'])
-        sentence_a = sentence_q.replace("...", '<strong>%s</strong>' % verb_form)
+if __name__ == "__main__":
 
-        if verb_form == form:
-            rule_p = "<strong>verben</strong> - <strong>%s %s</strong>" % (
-                pron, forms("verben")[pron])
-            rule_r = "<strong>%s</strong> - <strong>%s %s</strong>" % (verb, pron, verb_form)
-        else:
-            rule_r = rule_p = "<em>ir</em> <strong class='irregular'>%s</strong>" % verb_form
+    data = []
+    for verb, item in Varbs.items():
+        for pron, form in present_indicative(verb).items():
+            verb_form = form if pron not in item['extra'] else item['extra'][pron]
+            sentence_q = "%s ... %s" % (pron, item['addition'])
+            sentence_a = sentence_q.replace("...", '<strong>%s</strong>' % verb_form)
 
-        data.append((verb, item['translation'], sentence_q, sentence_a, rule_p, rule_r))
+            if verb_form == form:
+                rule_p = "<strong>verben</strong> - <strong>%s %s</strong>" % (
+                    pron, forms("verben")[pron])
+                rule_r = "<strong>%s</strong> - <strong>%s %s</strong>" % (verb, pron, verb_form)
+            else:
+                rule_r = rule_p = "<em>ir</em> <strong class='irregular'>%s</strong>" % verb_form
 
-deck = ed.Deck("Duo.Deu.Basics_Verbs")
+            data.append((verb, item['translation'], sentence_q, sentence_a, rule_p, rule_r))
 
-shuffle(data)
-for item in data:
-    deck.card('verbs', *item)
+    shuffle(data)
 
-deck.save(__file__.replace(".py", ""))
+    deck = ed.Deck("Duo.Deu.Basics_Verbs")
+    [deck.card('verbs', *item) for item in data]
+    deck.save(__file__.replace(".py", ""))
